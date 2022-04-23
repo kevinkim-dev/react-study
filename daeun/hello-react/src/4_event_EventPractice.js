0b23508452c5f1df775559b68f410f993e63dc04
박다은 <park.daeun@navercorp.com>
2022-04-23 23:39:55 +0900

    0423 ~chapter9

---
+++ daeun/hello-react/src/4_event_EventPractice.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const EventPractice = () => {
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const onChangeUsername = (e) => setUsername(e.target.value);
+  const onChangeMessage = (e) => setMessage(e.target.value);
+  const onClick = () => {
+    alert(username + ": " + message);
+    setUsername("");
+    setMessage("");
+  };
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  };
+  return <div></div>;
+};
+
+export default EventPractice;
